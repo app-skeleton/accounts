6c5531d42cd8a472bf4e00db03e1fdfefaace343
Pap Tamas <paptamas@gmail.com>
2013-08-21 14:22:12 +0300

    Create the config file for plans.

---
+++ config/account/plans.php
@@ -0,0 +1,30 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+return array(
+    // Plans
+    'trial' => array(
+        'limits' => array(
+            'projects' => 40
+        ),
+
+        // Time limit in days
+        'time_limit' => 90
+    ),
+
+    'starter' => array(
+        'limits' => array(
+            'projects' => 40
+        )
+    ),
+
+    'pro' => array(
+        'limits' => array(
+            'projects' => 100
+        )
+    ),
+
+    'unlimited' => array(
+        'limits' => array(
+            'projects' => INF
+        )
+    )
+);
