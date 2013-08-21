5af11556d8f2c88e3dc070805d57c627e20b9ee2
Pap Tamas <paptamas@gmail.com>
2013-08-21 14:13:54 +0300

    Implement the Project Deletion Request model.

---
+++ classes/Kohana/Model/Project/Deletion/Request.php
@@ -0,0 +1,36 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/*
+ * @package		Accounts Module
+ * @author      Pap Tamas
+ * @copyright   (c) 2011-2013 Pap Tamas
+ * @website		https://bitbucket.org/paptamas/kohana-accounts
+ * @license		http://www.opensource.org/licenses/isc-license.txt
+ *
+ */
+
+class Kohana_Model_Project_Deletion_Request extends ORM {
+
+    protected $_table_name = 'project_deletion_requests';
+
+    protected $_primary_key = 'request_id';
+
+    protected $_table_columns = array(
+        'request_id' => array(),
+        'project_id' => array(),
+        'requested_on' => array()
+    );
+
+    /**
+     * Delete all project deletion requests for the given project
+     *
+     * @param   int     $project_id
+     */
+    public function delete_all($project_id)
+    {
+        DB::delete($this->_table_name)
+            ->where('project_id', '=', DB::expr($project_id))
+            ->execute();
+    }
+}
+
+// END Kohana_Model_Project_Deletion_Request
