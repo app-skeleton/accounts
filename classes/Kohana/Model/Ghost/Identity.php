c7c4dd607c5ae6225ed715578f414878658a7471
Pap Tamas <paptamas@gmail.com>
2013-09-24 15:39:59 +0300

    Refactor the Ghost Identity Model.

--- classes/Kohana/Model/Ghost/Identity.php
+++ classes/Kohana/Model/Ghost/Identity.php
@@ -10,25 +10,6 @@
 
 class Kohana_Model_Ghost_Identity extends Model_Identity {
 
-    /**
-     * Define validation rules
-     *
-     * @return  array
-     */
-    public function rules()
-    {
-        return array();
-    }
-
-    /**
-     * Define filters
-     *
-     * @return  array
-     */
-    public function filters()
-    {
-        return array();
-    }
 }
 
 // END Kohana_Model_Ghost_Identity
