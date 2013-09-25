2fd3e82204928ff534f3dc9a268e910821eefdf0
Pap Tamas <paptamas@gmail.com>
2013-09-25 13:54:53 +0300

    Add the E_INVITATION_EMAIL_MISMATCH error constant to the Account Exception class.

--- classes/Kohana/Account/Exception.php
+++ classes/Kohana/Account/Exception.php
@@ -15,12 +15,13 @@ class Kohana_Account_Exception extends Kohana_Exception {
      */
     const E_USER_HAS_ACCOUNT            = 51;
     const E_INVITATION_LINK_INVALID     = 52;
-    const E_ACCESS_DENIED               = 53;
-    const E_INSUFFICIENT_PERMISSIONS    = 54;
-    const E_SUBSCRIPTION_IS_CANCELED    = 55;
-    const E_SUBSCRIPTION_IS_PAUSED      = 56;
-    const E_SUBSCRIPTION_IS_EXPIRED     = 57;
-    const E_OWNER_CAN_NOT_BE_REMOVED    = 58;
+    const E_INVITATION_EMAIL_MISMATCH   = 53;
+    const E_ACCESS_DENIED               = 54;
+    const E_INSUFFICIENT_PERMISSIONS    = 55;
+    const E_SUBSCRIPTION_IS_CANCELED    = 56;
+    const E_SUBSCRIPTION_IS_PAUSED      = 57;
+    const E_SUBSCRIPTION_IS_EXPIRED     = 58;
+    const E_OWNER_CAN_NOT_BE_REMOVED    = 59;
 
     /**
      * @var array   Default error messages
@@ -28,12 +29,13 @@ class Kohana_Account_Exception extends Kohana_Exception {
     public static $default_error_messages = array(
         51  => 'The user has an account already, and can not create a new one.',
         52  => 'The specified invitation link is expired on invalid.',
-        53  => 'The user has no access to the specified resource.',
-        54  => 'The user has no sufficient permissions to perform this action.',
-        55  => 'The specified account is canceled.',
-        56  => 'The specified account is paused.',
-        57  => 'The specified account is expired.',
-        58  => 'The account owner can not be removed from the account.'
+        53  => 'The specified invitation link is not for this user.',
+        54  => 'The user has no access to the specified resource.',
+        55  => 'The user has no sufficient permissions to perform this action.',
+        56  => 'The specified account is canceled.',
+        57  => 'The specified account is paused.',
+        58  => 'The specified account is expired.',
+        59  => 'The account owner can not be removed from the account.'
     );
 
     /**
